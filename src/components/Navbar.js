63e2c0145a4cd046dd76dd66a2af215e90d00097
amira <amiraabouhadid@gmail.com>
2022-03-22 14:51:57 +0200

    add navbar toggle and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const links = [
     {
       id: 1,
@@ -13,13 +16,28 @@ const Navbar = () => {
       text: "About",
     },
   ];
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
   return (
     <nav className="navBar">
-      <ul>
+      <button onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+        ) : (
+          <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         {links.map((link) => {
           return (
             <li key={link.id}>
-              <NavLink to={link.path}>{link.text}</NavLink>
+              <NavLink to={link.path} onClick={() => closeMenu()}>
+                {link.text}
+              </NavLink>
             </li>
           );
         })}
