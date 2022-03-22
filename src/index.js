5e651be9635c14778bcbea86bee1f46b3843fe2c
amira <amiraabouhadid@gmail.com>
2022-03-22 14:34:47 +0200

    add react router navbar

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import TodoContainer from "./components/TodoContainer";
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.render(
+  <Router>
+    <TodoContainer />{" "}
+  </Router>,
+  document.getElementById("root")
+);
